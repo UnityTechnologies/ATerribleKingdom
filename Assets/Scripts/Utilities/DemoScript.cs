5c42aa1bad40235b1e56c61bc201e7b4f00d758c
Ciro Continisio <ciro@unity3d.com>
2017-09-01 15:06:40 +0100

    Further refinements to Platoon timeline

---
+++ Assets/Scripts/Utilities/DemoScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+	public GameObject testUnits, testMonsters;
+	public GameObject flashbackTimeline, stormTimeline, aicommandTimeline;
+	public GameObject normalCamera, flashbackCamera;
+	public GameObject dialoguePanel;
+
+
+	void Update ()
+	{
+		//Gameplay
+		if(Input.GetKeyDown(KeyCode.Alpha0))
+		{
+			testUnits.SetActive(true);
+			testMonsters.SetActive(true);
+
+			flashbackTimeline.SetActive(false);
+			stormTimeline.SetActive(false);
+			aicommandTimeline.SetActive(false);
+
+			normalCamera.SetActive(true);
+			flashbackCamera.SetActive(false);
+
+			dialoguePanel.SetActive(false);
+		}
+
+		//Flashback timeline
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			testUnits.SetActive(false);
+			testMonsters.SetActive(false);
+
+			flashbackTimeline.SetActive(true);
+			stormTimeline.SetActive(false);
+			aicommandTimeline.SetActive(false);
+
+			normalCamera.SetActive(false);
+			flashbackCamera.SetActive(true);
+
+			//dialoguePanel.SetActive(false);
+		}
+
+		//Storm Timeline
+		if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			testUnits.SetActive(false);
+			testMonsters.SetActive(false);
+
+			flashbackTimeline.SetActive(false);
+			stormTimeline.SetActive(true);
+			aicommandTimeline.SetActive(false);
+
+			normalCamera.SetActive(true);
+			flashbackCamera.SetActive(false);
+
+			dialoguePanel.SetActive(false);
+		}
+
+		//Platoons Timeline
+		if(Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			testUnits.SetActive(false);
+			testMonsters.SetActive(false);
+
+			flashbackTimeline.SetActive(false);
+			stormTimeline.SetActive(false);
+			aicommandTimeline.SetActive(true);
+
+			normalCamera.SetActive(true);
+			flashbackCamera.SetActive(false);
+
+			dialoguePanel.SetActive(false);
+		}
+	}
+}
