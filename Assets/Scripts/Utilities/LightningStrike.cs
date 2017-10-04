b96bbb9a90191b7fbd5975f788f81476a33e5b3d
Ciro Continisio <ciro@unity3d.com>
2017-10-04 08:37:37 -0500

    Added audio

---
+++ Assets/Scripts/Utilities/LightningStrike.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningStrike : MonoBehaviour {
+
+	public AudioClip[] thunders;
+
+	void Start ()
+	{
+		AudioSource audioSource = GetComponent<AudioSource>();
+		audioSource.clip = thunders[Random.Range(0, 2)];
+		audioSource.pitch = Random.Range(.9f, 1.1f);
+		audioSource.Play();
+	}
+	
+
+}
